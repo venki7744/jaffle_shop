de5de8d3d10466cb93dc5c7126d282d1cb911c7c
Venki <venkitesh.r.mallan@gmail.com>
2022-04-04 00:01:18 +0530

    Modified Schema Generation
    Prefix Schema with DB User if target Dev. This would allow parallel development.

--- jaffle_shop-main/macros/custom_macros/generate_schema_name.sql
+++ jaffle_shop-main/macros/custom_macros/generate_schema_name.sql
@@ -1,14 +1,19 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
+    {% if target.name == 'dev' %}
+        {% set prefix = target.user %}
+    {% else %}
+        {% set prefix = none %}
+    {% endif %}
+    {% set final_schema = default_schema %}
+    {%- if custom_schema_name is not none -%}
+            {% set final_schema = custom_schema_name | trim %}
     {%- endif -%}
+    {% if prefix is not none %}
+        {% set final_schema =  prefix ~ '_' ~ final_schema %}
+    {% endif %}
+
+    {{ final_schema }}
 
 {%- endmacro %}
